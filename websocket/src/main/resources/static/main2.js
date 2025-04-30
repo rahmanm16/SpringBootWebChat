6008a0468c4eec05481fd2bbd395a75548557be8
Masroor Rahman <masroormainwork81@gmail.com>
2025-04-30 19:03:39 +0100

    major acsblty features added

---
+++ websocket/src/main/resources/static/main2.js
@@ -0,0 +1,6 @@
+IOTask<HttpResponse<String>> task = FXGL.getService(HttpClientService.class)
+    .sendGETRequestTask("https://example.com/api/data");
+
+
+    IOTask<HttpResponse<String>> task = FXGL.getService(HttpClientService.class)
+        .sendGETRequestTask("https://filler.co.uk/api/data");
